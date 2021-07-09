27b9d725e62e9d08c2ccecfa08bb5e597f7533b8
Emy <emyzheng80@gmail.com>
2021-07-09 09:41:19 -0400

    improve  the interface more friendly

--- rockPaperScissorsShootGame.py
+++ rockPaperScissorsShootGame.py
@@ -1,26 +1,38 @@
 import random
 deck = ["rock", "paper", "scissors"]
-start_game = (input("Would like play rock-paper-scissors game now, yes or no?\n")).lower()
+start_game = 'y'
 
-while start_game == 'yes':
-    user_shoot = input("What are going to shoot this run, rock, paper, or scissors?\n").lower()
-    user_shoot_index = deck.index(user_shoot)
-    computer_shoot_index = random.randint(0, 2)
-    computer_shoot = deck[computer_shoot_index]
-    print(f"Here is the computer shoot:\n{computer_shoot}")
+while start_game == 'y':
+    start_game = (input("Game over. Play again, 'Y' or 'y' for yes or others for no?\n")).lower()
 
-    if user_shoot == computer_shoot:
-        print ("You won a draw.")
-    else:
-        if user_shoot_index < 2:
-            if user_shoot_index < computer_shoot_index:
-                print("You lose.")
-            else:
-                print("You won.")  
-        else:
-            if computer_shoot_index == 0:
-                print("You lose.")
+    if start_game == 'y':
+        print('\n************************')
+        print('     starting Game!')
+        print('************************\n')
+
+        user_shoot_index = int(input("What are going to shoot this run, 0 fro rock, 1 for paper, or 2 for scissors?\n\n"))
+        
+        computer_shoot_index = random.randint(0, 2)
+        computer_shoot = deck[computer_shoot_index]
+        
+        if user_shoot_index < 0 or user_shoot_index > 2:
+            print("invalid number. You lose!")
+        elif user_shoot_index == computer_shoot_index:
+            print("You won a draw.")
+        elif user_shoot_index > computer_shoot_index:
+            if user_shoot_index == 2 and computer_shoot_index == 0:
+                print("You lose")
             else:
                 print("You won.")
+        elif user_shoot_index < computer_shoot_index:
+            if user_shoot_index == 0 and computer_shoot_index == 2:
+                print("You won.")
+            else:
+                print("You lose")
 
-    start_game = (input("Game over. Play again, yes or no?\n")).lower()
+        print('************************')
+        print(f"{deck[user_shoot_index]} VS {computer_shoot}")
+        print('************************\n')
+            
+    else:
+        print('Good bye and see you next time!')   
