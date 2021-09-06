9202bc0d985dea3546ce895e0c9c60603ed12e49
Emy <emyzheng80@gmail.com>
2021-09-06 15:40:20 -0400

    add f string and format to handle print

--- stringSlice.py
+++ stringSlice.py
@@ -1,10 +1,40 @@
 my_string = 'tinker'
 
-#start the second character and stop the fifth but not include
-print(my_string[1:4])
+# Strings are not mutable! (meaning you can't use indexing to change individual elements of a string) 
+# my_string[0] = 'x'
 
-#from the beginning and stop to the fourth but not include
-print(my_string[:4])
+# start the second character and stop the fifth but not include
+print(my_string[1:4]) #tink
 
-#slice characters by step 3
-print(my_string[::3])
+# from the beginning and stop to the fourth but not include
+print(my_string[:4]) # tink
+
+# slice characters by step 3
+print(my_string[::3]) # tk
+
+print(my_string * 10) # tinkertinkertinkertinkertinkertinkertinkertinkertinkertinker
+
+print('2' + '3') # 23
+
+print(2 + 3)  # 5
+
+print(my_string.split('i')) #['t', 'nker']
+
+print('This is a string {}'.format('INSERTED')) # This is a string INSERTED
+
+print('The {} {} {}'.format('fox', 'brown', 'quick')) # the fox brown quick
+
+print('The {q} {b} {f}'.format(f ='fox', b ='brown', q ='quick')) # the quick brown fox 
+
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick')) # the quick brown fox
+
+print('The {0} {0} {0}'.format('fox', 'brown', 'quick')) # the fox fox fox
+
+# float formatting follows "{value:width.precision f}" width is the white space
+
+result = 10000/23
+
+print('The result was {r:1.3f}'.format(r = result)) # The result was 434.783
+
+name = 'Jose'
+print(f'Hello, my name is {name}') # f_string 
