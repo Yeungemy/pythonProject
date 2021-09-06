86b5ec299fa2cc6fe93fdce42df787bb9a594364
Emy <emyzheng80@gmail.com>
2021-09-06 15:05:11 -0400

    add comments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Run file: python3 fileName
+git add .
+git commit -m "message"
+git push
--- stringSlice.py
+++ stringSlice.py
@@ -1,2 +1,10 @@
 my_string = 'tinker'
+
+#start the second character and stop the fifth but not include
 print(my_string[1:4])
+
+#from the beginning and stop to the fourth but not include
+print(my_string[:4])
+
+#slice characters by step 3
+print(my_string[::3])
